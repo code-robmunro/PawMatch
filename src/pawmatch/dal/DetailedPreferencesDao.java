bc01b71ece1c11083d54e5fd98c83d06f0b35c33
Irena Kushner <kushner.ir@husky.neu.edu>
2017-12-01 16:26:20 -0800

    Method to find detailedpreferences by userid

--- src/pawmatch/dal/DetailedPreferencesDao.java
+++ src/pawmatch/dal/DetailedPreferencesDao.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import pawmatch.model.DetailedPreferences;
+import pawmatch.model.Enums;
 
 /**
  * Created by Robert Munro on 11/12/2017.
@@ -157,4 +158,81 @@ public class DetailedPreferencesDao extends AbstractDao {
       }
     }
   }
+
+  public DetailedPreferences getDetailedPrefsByUserId(int userId) throws SQLException {
+    String selectDetailedprefs =
+        "SELECT * FROM DetailedPreferences WHERE userId=?;";
+    Connection connection = null;
+    PreparedStatement selectStmt = null;
+    ResultSet results = null;
+    try {
+      connection = connectionManager.getConnection();
+      selectStmt = connection.prepareStatement(selectDetailedprefs);
+      selectStmt.setInt(1, userId);
+      results = selectStmt.executeQuery();
+      if(results.next()) {
+        Integer detailedPreferencesId = results.getInt("DetailedPreferencesId");
+        Boolean okDogs = results.getBoolean("OKWithDogs");
+        Boolean okCats = results.getBoolean("OKWithCats");
+        Boolean okKids = results.getBoolean("OKWithKids");
+        Boolean okAdults = results.getBoolean("OKWithAdults");
+        Boolean okFarm = results.getBoolean("OKWithFarm");
+        Boolean seniors = results.getBoolean("GoodWithSeniors");
+        Boolean declawed = results.getBoolean("Declawed");
+        String color = results.getString("Color");
+        Boolean upToDate = results.getBoolean("UpToDate");
+        Boolean pictures = results.getBoolean("Pictures");
+        Boolean videos = results.getBoolean("Videos");
+        Boolean obedience = results.getBoolean("ObedienceTraining");
+        Integer fee = results.getInt("Fee");
+        Boolean exercise = results.getBoolean("Exercise");
+        Enums.EnergyLevel energyLevel = Enums.EnergyLevel.valueOf(parseValue(results, "EnergyLevel"));
+        Enums.ActivityLevel activityLevel = Enums.ActivityLevel.valueOf(parseValue(results, "ActivityLevel"));
+        Boolean grooming = results.getBoolean("GroomingNeeds");
+        Boolean shedding = results.getBoolean("Shedding");
+        Boolean goofy = results.getBoolean("Goofy");
+        Boolean hypoallergenic = results.getBoolean("Hypoallergenic");
+        Boolean carTrained = results.getBoolean("CarTrained");
+        Boolean leashTrained = results.getBoolean("LeashTrained");
+        Boolean likesToFetch = results.getBoolean("LikesToFetch");
+        Boolean likesToys = results.getBoolean("LikesToys");
+        Boolean likesSwimming = results.getBoolean("LikesSwimming");
+        Boolean likesLaps = results.getBoolean("LikesLaps");
+        Boolean apartment = results.getBoolean("Apartment");
+        Boolean protective = results.getBoolean("Protective");
+        Boolean obedient = results.getBoolean("Obedient");
+        Boolean playful = results.getBoolean("Playful");
+        Boolean timidShy = results.getBoolean("TimidShy");
+        Boolean independent = results.getBoolean("Independent");
+        Boolean affectionate = results.getBoolean("Affectionate");
+        Boolean eagerToPlease = results.getBoolean("EagerToPlease");
+        Boolean evenTempered = results.getBoolean("EvenTempered");
+        Boolean gentle = results.getBoolean("Gentle");
+        Integer resultUserId = results.getInt("UserId");
+
+        DetailedPreferences prefs = new DetailedPreferences(detailedPreferencesId, okDogs, okCats, okKids,
+            okAdults, okFarm, seniors, declawed, color, upToDate, pictures, videos, obedience, fee,
+            exercise, energyLevel, activityLevel, grooming, shedding, goofy, hypoallergenic, carTrained,
+            leashTrained, likesToFetch, likesToys, likesSwimming, likesLaps, apartment, protective,
+            obedient, playful, timidShy, independent, affectionate, eagerToPlease, evenTempered,
+            gentle, resultUserId);
+        return prefs;
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+      throw e;
+    } finally {
+      if(connection != null) {
+        connection.close();
+      }
+      if(selectStmt != null) {
+        selectStmt.close();
+      }
+      if(results != null) {
+        results.close();
+      }
+    }
+    return null;
+  }
+
 }
