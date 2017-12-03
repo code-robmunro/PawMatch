b264215441a53676608389181b2e48988f08b79f
Irena Kushner <kushner.ir@husky.neu.edu>
2017-12-02 17:00:03 -0800

    Updating script to add detailed preference columns to PetProfiles

--- Scripts/Fill_In_Custom_Tables.sql
+++ Scripts/Fill_In_Custom_Tables.sql
@@ -1,11 +1,21 @@
-#  TODO fill in ShelteredLonger, PicturesId, VideosId, ShelterProfileId
+#  TODO fill in ShelteredLonger, PicturesId, VideosId, ShelterProfileId, Fee
 INSERT INTO PetProfiles (
-Species, Sex, Breed, Age, Size, HouseTrained, CoatLength, Location)
-		(SELECT Species, Sex, Breed, Age, Size, HouseTrained, CoatLength, AnimalLocation
+Species, Sex, Breed, Age, Size, HouseTrained, CoatLength, Location, Color, OKWithDogs, OKWithCats, OKWithKids,
+	Declawed, OKWithAdults, OKWithFarm, GoodWithSeniors, UpToDate, ObedienceTraining, ExerciseNeeds,
+    EnergyLevel, ActivityLevel, GroomingNeeds, Shedding, Goofy, Hypoallergenic, CarTrained, LeashTrained,
+    LikesToFetch, LikesToys, LikesSwimming, LikesLaps, Apartment,  Protective, Obedient, Playful, TimidShy, 
+    Independent, Affectionate, EagerToPlease, EvenTempered, Gentle)
+		(SELECT Species, Sex, Breed, Age, Size, HouseTrained, CoatLength, AnimalLocation, Color, Dogs, Cats, Kids,
+        Declawed, OKWithAdults, OKWithFarmAnimals, OKForSeniors, UpToDate, ObedienceTraining, ExerciseNeeds,
+        EnergyLevel, ActivityLevel, GroomingNeeds, Shedding, Goofy, Hypoallergenic, GoodInCar, LeashTrained, 
+        Fetches, PlaysToys, Swims, Lap, Apartment, Protective, Obedient, Playful, Timid, Independent, Affectionate,
+        EagerToPlease, EvenTempered, Gentle
         FROM Animals 
         WHERE Age IN ('Adult', 'Baby', 'Senior', 'Young', '')
 			AND CoatLength IN ('Short', 'Medium', 'Long', '')
             AND Sex IN ('Male', 'Female', '')
+            AND EnergyLevel IN('High', 'Low', 'Moderate', '')
+            AND ActivityLevel IN('Not Active', 'Slightly Active', 'Moderately Active', 'Highly Active', '')
         );
         
 INSERT INTO Users (UserName, FirstName, LastName, Email, Password, SimplePreferencesId, DetailedPreferencesId, NotificationsOn, Foster)
