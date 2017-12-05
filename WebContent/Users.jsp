08e10138e83f668460fbb7e12a31e1bf011987d2
Irena Kushner <kushner.ir@husky.neu.edu>
2017-12-04 17:56:28 -0800

    Landing page for searching user

---
+++ WebContent/Users.jsp
@@ -0,0 +1,59 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
+
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Users</title>
+</head>
+<body>
+	<form action="users" method="post">
+		<h1>Search for a User by User ID</h1>
+		<p>
+			<label for="userid">User ID</label>
+			<input id="userid" name="userid" value="${fn:escapeXml(param.userid)}">
+		</p>
+		<p>
+			<input type="submit">
+			<br/><br/><br/>
+			<span id="successMessage"><b>${messages.success}</b></span>
+		</p>
+	</form>
+	<br/>
+	<div id="usercreate"><a href="usercreate">Create User</a></div>
+	<br/>
+	<h1>Matching User</h1>
+        <table border="1">
+            <tr>
+                <th>ID</th>
+                <th>UserName</th>
+                <th>FirstName</th>
+				<th>LastName</th>
+				<th>Email</th>
+				<th>Password</th>
+				<th>SimplePreferencesId</th>
+				<th>DetailedPreferencesId</th>
+				<th>NotificationsOn</th>
+				<th>Foster</th>
+            </tr>
+                <tr>
+                    <td><c:out value="${user.getUserId()}" /></td>
+                    <td><c:out value="${user.getUserName()}" /></td>
+                    <td><c:out value="${user.getFirstName()}" /></td>
+                    <td><c:out value="${user.getLastName()}" /></td>
+                    <td><c:out value="${user.getEmail()}" /></td>
+                    <td><c:out value="${user.getPassword()}" /></td>
+                    <td><c:out value="${user.getSimplePreferencesId()}" /></td>
+                    <td><c:out value="${user.getDetailedPreferencesId()}" /></td>
+                    <td><c:out value="${user.getNotificationsOn()}" /></td>
+                    <td><c:out value="${user.getFoster()}" /></td>
+                  	<td><a href="userdelete?userid=<c:out value="${user.getUserId()}"/>">Delete</a></td>
+                    <%--<td><a href="userupdate?userid=<c:out value="${commonApp.getUserId()}"/>">Update</a></td>--%>
+                </tr>
+       </table>
+</body>
+</html>
---
+++ src/pawmatch/servlet/UsersServlet.java
@@ -0,0 +1,92 @@
+package pawmatch.servlet;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import pawmatch.dal.UsersDao;
+import pawmatch.model.Users;
+
+
+/**
+ * CommonApplication is the primary entry point into the application.
+ */
+@WebServlet("/users")
+public class UsersServlet extends HttpServlet {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3947916779464848110L;
+	protected UsersDao usersDao;
+	
+	@Override
+	public void init() throws ServletException {
+		usersDao = UsersDao.getInstance();
+	}
+	
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// Map for storing messages.
+        Map<String, String> messages = new HashMap<String, String>();
+        req.setAttribute("messages", messages);
+
+        Users user = null;
+        
+        String userId = req.getParameter("userid");
+        if (userId == null || userId.trim().isEmpty()) {
+            messages.put("success", "Please enter a valid user ID.");
+        } else {
+        	try {
+        		user = usersDao.getUserById(Integer.parseInt(userId));
+            } catch (SQLException e) {
+    			e.printStackTrace();
+    			throw new IOException(e);
+            }
+        	messages.put("success", "Displaying results for user ID " + userId);
+        	// Save the previous search term, so it can be used as the default
+        	// in the input box when rendering FindUsers.jsp.
+        	messages.put("previous user ID", userId.toString());
+        }
+        req.setAttribute("user", user);
+        
+        req.getRequestDispatcher("/Users.jsp").forward(req, resp);
+	}
+	
+	@Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp)
+    		throws ServletException, IOException {
+		// Map for storing messages.
+        Map<String, String> messages = new HashMap<String, String>();
+        req.setAttribute("messages", messages);
+
+        Users user = null;
+        
+        String userId = req.getParameter("userid");
+        if (userId == null || userId.trim().isEmpty()) {
+            messages.put("success", "Please enter a valid user ID.");
+        } else {
+        	try {
+        		user = usersDao.getUserById(Integer.parseInt(userId));
+            } catch (SQLException e) {
+    			e.printStackTrace();
+    			throw new IOException(e);
+            }
+        	messages.put("success", "Displaying results for user ID: " + userId);
+        	// Save the previous search term, so it can be used as the default
+        	// in the input box when rendering FindUsers.jsp.
+        	messages.put("previous user ID", userId.toString());
+        }
+        req.setAttribute("user", user);
+        
+        req.getRequestDispatcher("/Users.jsp").forward(req, resp);
+	}
+}
